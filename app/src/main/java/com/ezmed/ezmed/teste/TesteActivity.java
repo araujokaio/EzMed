b204d4f0517d2cd8f6092b566a495512121b7c4e
araujokaio <kaio.monteiro.araujo@gmail.com>
2017-10-14 19:13:30 -0300

    Create test module and adjust some activities layout

---
+++ app/src/main/java/com/ezmed/ezmed/teste/TesteActivity.java
@@ -0,0 +1,150 @@
+package com.ezmed.ezmed.teste;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.ezmed.ezmed.LoginActivity;
+import com.ezmed.ezmed.MainActivity;
+import com.ezmed.ezmed.R;
+import com.ezmed.ezmed.RecuperarSenhaActivity;
+import com.ezmed.ezmed.RegistrarActivity;
+import com.ezmed.ezmed.core.Login;
+import com.ezmed.ezmed.criacao.CriarAlertaActivity;
+import com.ezmed.ezmed.criacao.CriarCuidadorActivity;
+import com.ezmed.ezmed.criacao.CriarMedicamentoActivity;
+import com.ezmed.ezmed.criacao.CriarPacienteActivity;
+import com.ezmed.ezmed.criacao.CriarTratamentoActivity;
+import com.ezmed.ezmed.gerenciamento.GerenciarCuidadorActivity;
+import com.ezmed.ezmed.gerenciamento.GerenciarMedicamentoActivity;
+import com.ezmed.ezmed.gerenciamento.GerenciarPacienteActivity;
+import com.ezmed.ezmed.gerenciamento.GerenciarTratamentoActivity;
+
+public class TesteActivity extends AppCompatActivity
+{
+
+    Button btnCriarAlerta;
+    Button btnCriarCuidador;
+    Button btnCriarMedicamento;
+    Button btnCriarPaciente;
+    Button btnCriarTratamento;
+    Button btnGerenciarCuidador;
+    Button btnGerenciarMedicamento;
+    Button btnGerenciarPaciente;
+    Button btnGerenciarTratamento;
+    Button btnLogin;
+    Button btnMain;
+    Button btnRecuperarSenha;
+    Button btnRegistrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_teste);
+
+        setButtonsLocal();
+        setOnClickListenerButtons();
+    }
+
+    private void setOnClickListenerButtons()
+    {
+        btnCriarAlerta.setOnClickListener((View v) ->
+        {
+            Intent i = new Intent(getApplicationContext(), CriarAlertaActivity.class);
+            startActivity(i);
+        });
+
+        btnCriarCuidador.setOnClickListener((View v) ->
+        {
+            Intent i = new Intent(getApplicationContext(), CriarCuidadorActivity.class);
+            startActivity(i);
+        });
+
+        btnCriarMedicamento.setOnClickListener((View v) ->
+        {
+            Intent i = new Intent(getApplicationContext(), CriarMedicamentoActivity.class);
+            startActivity(i);
+        });
+
+        btnCriarPaciente.setOnClickListener((View v) ->
+        {
+            Intent i = new Intent(getApplicationContext(), CriarPacienteActivity.class);
+            startActivity(i);
+        });
+
+        btnCriarTratamento.setOnClickListener((View v) ->
+        {
+            Intent i = new Intent(getApplicationContext(), CriarTratamentoActivity.class);
+            startActivity(i);
+        });
+
+        btnGerenciarCuidador.setOnClickListener((View v) ->
+        {
+            Intent i = new Intent(getApplicationContext(), GerenciarCuidadorActivity.class);
+            startActivity(i);
+        });
+
+        btnGerenciarMedicamento.setOnClickListener((View v) ->
+        {
+            Intent i = new Intent(getApplicationContext(), GerenciarMedicamentoActivity.class);
+            startActivity(i);
+        });
+
+        btnGerenciarPaciente.setOnClickListener((View v) ->
+        {
+            Intent i = new Intent(getApplicationContext(), GerenciarPacienteActivity.class);
+            startActivity(i);
+        });
+
+        btnGerenciarTratamento.setOnClickListener((View v) ->
+        {
+            Intent i = new Intent(getApplicationContext(), GerenciarTratamentoActivity.class);
+            startActivity(i);
+        });
+
+        btnLogin.setOnClickListener((View v) ->
+        {
+            Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(i);
+        });
+
+        btnMain.setOnClickListener((View v) ->
+        {
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(i);
+        });
+
+        btnRecuperarSenha.setOnClickListener((View v) ->
+        {
+            Intent i = new Intent(getApplicationContext(), RecuperarSenhaActivity.class);
+            startActivity(i);
+        });
+
+        btnRegistrar.setOnClickListener((View v) ->
+        {
+            Intent i = new Intent(getApplicationContext(), RegistrarActivity.class);
+            startActivity(i);
+        });
+    }
+
+    private void setButtonsLocal()
+    {
+        btnCriarAlerta = (Button) findViewById(R.id.testeBtnCriarAlerta);
+        btnCriarCuidador = (Button) findViewById(R.id.testeBtnCriarCuidador);
+        btnCriarMedicamento = (Button) findViewById(R.id.testeBtnCriarMedicamento);
+        btnCriarPaciente = (Button) findViewById(R.id.testeBtnCriarPaciente);
+        btnCriarTratamento = (Button) findViewById(R.id.testeBtnCriarTratamento);
+        btnGerenciarCuidador = (Button) findViewById(R.id.testeBtnGerenciarCuidador);
+        btnGerenciarMedicamento = (Button) findViewById(R.id.testeBtnGerenciarMedicamento);
+        btnGerenciarPaciente = (Button) findViewById(R.id.testeBtnGerenciarPaciente);
+        btnGerenciarTratamento = (Button) findViewById(R.id.testeBtnGerenciarTratamento);
+        btnLogin = (Button) findViewById(R.id.testeBtnLogin);
+        btnMain = (Button) findViewById(R.id.testeBtnMain);
+        btnRecuperarSenha = (Button) findViewById(R.id.testeBtnRecuperarSenha);
+        btnRegistrar = (Button) findViewById(R.id.testeBtnRegistrar);
+    }
+}
